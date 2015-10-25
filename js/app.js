0a43e0a315aed6f39ce296584df080386beb8de8
weiliy <yi@weiliy.net>
2015-10-25 22:08:44 +0800

    feat: conflit with bugs

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
     this.x = -100;
     this.y = Math.floor(Math.random() * 3) * 75 + 75;
-    this.speed = 100 * Math.random() + 20;
+    this.speed = 150 * Math.random() + 20;
 };
 
 // Update the enemy's position, required method for game
@@ -17,9 +17,12 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = ( this.x + this.speed * dt ) % 500;
-    //var dis = Math.sqrt((this.x - player.x)**2 + (this.y - player.y));
-    //onsloe.log(dis);
+    this.x = ( this.x + this.speed * dt ) % 1000;
+    var dis = Math.sqrt(Math.pow((this.x - player.x),2) 
+            + Math.pow((this.y - player.y),2));
+    if ( dis < 70 ) {
+        player.reset();
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -37,8 +40,16 @@ var Player = function() {
     this.speed = 1;
 };
 
-Player.prototype.update = function(dt) {
+Player.prototype.reset = function() {
+    console.log( 'reset');
+    this.x = 200;
+    this.y = 300;
+};
 
+Player.prototype.update = function(dt) {
+    if ( this.y < 0 ) {
+        this.reset();
+    }
 };
 
 Player.prototype.render = function() {
@@ -63,16 +74,35 @@ Player.prototype.handleInput = function(key) {
             }
             break;
         case "down":
-            if (this.y < 300) {
+            if (this.y < 400) {
                 this.y += 80;
             }
     }
 };
+
+var Score = function() {
+    this.x = 0;
+    this.y = 400;
+    this.grade = 0;
+    this.timer = 0;
+};
+
+Score.prototype.update = function(dt) {
+    this.timer += 100 * dt;
+};
+
+Score.prototype.render = function() {
+    ctx.fillText( "Score: " + this.grade + "\n" +
+            "Timer: " + this.timer, this.x, this.y);
+};
+
+
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 var allEnemies = [];
-for (var i = 3; i >= 0; i--) {
+for (var i = 7; i >= 0; i--) {
     allEnemies.push(new Enemy);
 };
 
